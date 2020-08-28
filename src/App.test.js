ee40918447ed347a3ade93cfda52f396c311cd88
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-08-28 20:42:41 +0500

    App Updated

--- src/App.test.js
+++ src/App.test.js
@@ -13,3 +13,9 @@ test('text case', () => {
   const linkElement = getByText(/Hello PIAIC/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('text case', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello Everyone/i);
+  expect(linkElement).toBeInTheDocument();
+});
